51d10034b487311dfd22a52632da09c722e8bf1c
narasakka999 <sreeramulajunnu@gmail.com>
2025-06-16 10:14:48 +0530

    Add files via upload

---
+++ AlphabeticalOrder.java
@@ -0,0 +1,12 @@
+package Nuts;
+import java.util.Arrays;
+public class AlphabeticalOrder {
+
+	public static void main(String[] args) {
+		String [] words = {"banana","chocolate","dairymilk","apple"};
+		Arrays.sort(words);
+		System.out.println("sorted words:"+Arrays.toString(words));
+
+	}
+
+}
