6ec7ea28a5f790a82758bd0646d2323525b46b5e
narasakka999 <sreeramulajunnu@gmail.com>
2025-06-16 10:18:29 +0530

    Add files via upload

---
+++ ConstantTimeExample.java
@@ -0,0 +1,21 @@
+package Nuts;
+
+public class ConstantTimeExample {
+	public static  void printFirstElement(int[] arr)
+	{
+		if(arr.length>0)
+		{
+			System.out.println("first element is:"+arr[0]);
+		}
+		else {
+			System.out.println("Array is empty");
+		}
+	}
+	public static void main(String [] args)
+	{
+		int[] numbers = {10,20,30,40,50};
+		
+		printFirstElement(numbers);
+	}
+
+}
